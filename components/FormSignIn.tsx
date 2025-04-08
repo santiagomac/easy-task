ce9a780d7f16fda26876d38c41b52755c9770058
santiagomac <smazuerac@unac.edu.co>
2025-04-07 20:44:12 -0500

    add sign up page

--- components/FormSignIn.tsx
+++ components/FormSignIn.tsx
@@ -26,14 +26,18 @@ export const FormSignIn = () => {
       username: "",
       password: "",
     },
+    mode: "onChange",
   });
 
   const handleSubmit = (data: z.infer<typeof signInSchema>) => {
     console.log(data);
   };
 
+  console.log(form.formState.isValid);
+
   return (
     <div className="w-[350px]">
+      <h2 className="text-3xl font-bold text-center mb-4">Sign in</h2>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-4">
           <FormField
@@ -66,13 +70,19 @@ export const FormSignIn = () => {
               </FormItem>
             )}
           />
-          <p className="text-sm text-center text-muted-foreground">
+          <p className="text-sm text-center text-gray-600">
             Dont have an account?{" "}
-            <Link href="/signup" className="text-blue-500 font-semibold">
+            <Link
+              href="/sign-up"
+              className="text-blue-500 font-semibold hover:underline"
+            >
               Sign up
             </Link>
           </p>
-          <Button className="w-full bg-blue-500 hover:bg-blue-500/80 cursor-pointer">
+          <Button
+            className="w-full bg-blue-500 hover:bg-blue-500/80 cursor-pointer"
+            disabled={!form.formState.isValid}
+          >
             Sign in
           </Button>
         </form>
